b8770b151a72a3243e2f50459614b4f9a6cb8adb
0pashich <0pashich@gmail.com>
2022-09-02 00:07:48 +0300

    updateed

--- apps/api/src/app/news/news.service.ts
+++ apps/api/src/app/news/news.service.ts
@@ -1,20 +1,3 @@
-// import { Model } from 'mongoose';
-// import { Injectable } from '@nestjs/common';
-// import { InjectModel } from '@nestjs/mongoose';
-// import { News, NewsDocument } from './schemas/news.schema';
-// import { CreateNewsDto } from './dto/create-news.dto';
-// @Injectable()
-// export class NewsService {
-//   constructor(@InjectModel(News.name) private newsModel: Model<NewsDocument>) { }
-//   async create(createNewsDto: CreateNewsDto): Promise<News> {
-//     const createdNews = new this.newsModel(createNewsDto);
-//     return createdNews.save();
-//   }
-//   async findAll(): Promise<News[]> {
-//     return this.newsModel.find().exec();
-//   }
-// }
-
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
