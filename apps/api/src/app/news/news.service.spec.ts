e0d02efbfca376af2282d5cdd8098b6b1268babc
Pavel <0pashich@gmail.com>
2022-09-07 00:47:30 +0300

    modified test

--- apps/api/src/app/news/news.service.spec.ts
+++ apps/api/src/app/news/news.service.spec.ts
@@ -1,12 +1,14 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { NewsService } from './news.service';
+import { News } from './schemas/news.schema';
 
 describe('NewsService', () => {
   let service: NewsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NewsService],
+      providers: [NewsService, { provide: getModelToken(News.name), useValue: jest.fn() }],
     }).compile();
 
     service = module.get<NewsService>(NewsService);
